392c3e304c10951b084e029deae113acced0baa4
Randall <36777632+randallduke@users.noreply.github.com>
2023-05-10 21:56:41 +0000

    Update 3 files

--- src/App_with_yup.js
+++ src/App_with_yup.js
@@ -1,7 +1,7 @@
-import "./App.css";
 import { useForm } from "react-hook-form";
 import { schema } from "./schema";
 import { yupResolver } from "@hookform/resolvers/yup";
+import "./styles.css";
 
 function AppWithYup() {
   const {
@@ -11,29 +11,51 @@ function AppWithYup() {
   } = useForm({
     resolver: yupResolver(schema)
   });
-  const onSubmit = (data) => {
-    alert(data);
-  };
+  const onSubmit = (data) => console.log(data);
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* Username */}
-        <section>
-          <label>Enter username</label>
-          <input type="text" placeholder="username" {...register("username")} />
-          <span>{errors.username?.message}</span>
-        </section>
-        {/* Age */}
-        <section>
-          <label>Enter age</label>
-          <input type="number" placeholder="age" {...register("age")} />
-          <span>{errors.age?.message}</span>
-        </section>
-        {/* Submit button */}
-        <section>
-          <input type="submit" />
-        </section>
-      </form>
+    <div>
+      <h1 className="app-title">Bufex Form Example</h1>
+      <section className="card">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="section-header">React Hook Form</h2>
+          <div className="section-item">
+              <label>User Name : </label>
+              <input
+                {...register("userName")}
+                placeholder="User Name"
+              />
+              <p>{errors.userName?.message}</p>
+          </div>
+          <div className="section-item small-top-margin">
+            <label>Password : </label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Password"
+            />
+            <p>{errors.password?.message}</p>
+          </div>
+          <div className="section-item small-top-margin">
+            <label>Bufex Email : </label>
+            <input
+              {...register("email")}
+              placeholder="Email"
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div className="section-item small-top-margin">
+            <label>Cell Phone : </label>
+            <input
+              {...register("cellPhone")}
+              placeholder="555-555-5555"
+            />
+            <p>{errors.cellPhone?.message}</p>
+          </div>
+          <div className="section-item small-top-margin">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
