01cdfaacd721178c9634be532537976be91c1c24
Randall <36777632+randallduke@users.noreply.github.com>
2023-05-10 14:59:28 -0700

    Convert to use Yup (#1)

--- src/schema.js
+++ src/schema.js
@@ -2,15 +2,22 @@ import * as yup from "yup";
 
 export const schema = yup
   .object({
-    username: yup
+    userName: yup
       .string()
       .required("Username is required")
       .min(4, "Username must be at least 4 letters long")
       .max(10, "Username must not be 10 letters long"),
-    age: yup
-      .number()
-      .required("Age is required")
-      .max(70, "Age is too high")
-      .min(18, "Age is too small")
+    password: yup
+      .string()
+      .required("Password is required")
+      .matches(/^.{8,}$/, "Must be at least 8 characters long" ),
+    email: yup
+      .string()
+      .required("Email is required")
+      .matches(/^[a-zA-Z0-9._%+-]+@bufex+\.com$/, "Must be an @bufex.com email."),
+    cellPhone: yup
+      .string()
+      .required("Cell Phone is required")
+      .matches(/^\d{3}-\d{3}-\d{4}$/, "Please follow the format 555-555-5555")
   })
   .required();
